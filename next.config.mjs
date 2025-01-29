8aedce44a3d6d1d984337036a517ca941e777cf4
bishalbiswas-work <bishalbiswas.work@gmail.com>
2025-01-29 21:17:56 +0530

    new code

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    async redirects() {
+        return [];
+    },
+    functions: {
+        'api/ai-logo-model': {
+            maxDuration: 60, // Ensure it's between 1 and 60 seconds for Hobby plan
+        },
+    },
 };
 
 export default nextConfig;
